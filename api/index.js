15139a01f438c3c17b6b043fcb3c339f0c2f1853
Iliya Zhechev <i4ko95@gmail.com>
2017-06-10 02:50:55 +0300

    Todos refactoring

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 80;
 
 
-let todos = new class Todos {
-    constructor(){
+const todosRepository = new class TodosRepository {
+    constructor() {
         this.currentId = 0;
         this.db = new Map();
     }
 
     add(description) {
-        this.db.set(this.currentId, { description, done: false });
+        this.db.set(this.currentId++, { description, done: false });
         return this.db.length - 1;
     }
 
@@ -33,11 +33,12 @@ let todos = new class Todos {
     }
 };
 
-todos.add('Finish R homework');
-todos.add('Explore docker');
+todosRepository.add('Finish R homework');
+todosRepository.add('Explore docker');
 
-app.get('/todo', async (req, res) => {
-    const todos = await todos.getAll();
+
+app.get('/', async (req, res) => {
+    const todos = await todosRepository.getAll();
     res.json(todos);
 });
 
